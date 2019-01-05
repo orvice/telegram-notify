80f2ff6c2a84a477c52837a193b695fcd89bda7a
orvice <orvice@gmail.com>
2019-01-05 16:15:35 +0800

    update format

--- notify.sh
+++ notify.sh
@@ -2,6 +2,6 @@
 
 TIME="10"
 URL="https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage"
-TEXT="Deploy the status: $1 %0A%0A Project: + $CI_PROJECT_NAME  %0A%0A URL: + $CI_PROJECT_URL / Pipelines / $CI_PIPELINE_ID / %0A%0A Branch: + $CI_COMMIT_REF_SLUG "
+TEXT=" Deploy the status: $1 %0A%0A Project: + $CI_PROJECT_NAME  %0A%0A URL: + $CI_PROJECT_URL/Pipelines/$CI_PIPELINE_ID  %0A%0A Branch: + $CI_COMMIT_REF_SLUG "
 
 curl -s --max-time $TIME -d "chat_id=$TELEGRAM_CHATID&disable_web_page_preview=1&text=$TEXT"  $URL > /dev/null
